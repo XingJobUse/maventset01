27850b398c8935fe815e8917c2b652f35cfa9a7e
lhx <xingjob@126.com>
2022-09-12 01:13:07 +0800

    second commit

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        Integer i =  1;
-        System.out.println(i);
+        System.out.println("hello git1");
+        System.out.println("hello git2");
     }
 }
